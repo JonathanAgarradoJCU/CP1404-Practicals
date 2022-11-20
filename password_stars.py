2a08876869ad02c7951c7fc93de202aa6fd64245
jonathanroi.agarrado@my.jcu.edu.au <Romans12:9@>
2022-11-20 17:20:13 +0800

    Convert password input into stars

--- password_stars.py
+++ password_stars.py
@@ -1,10 +1,8 @@
 """
-
 On paper, write a program that asks the user for a password, with error-checking
 to repeat if the password doesn't meet a minimum length set by a variable.
 The program should then print asterisks as long as the word.
 Example: if the user enters Pythonista (10 characters), the program should print **********.
-
 """
 
 PASSWORD = 8  # minimum length of typical password
