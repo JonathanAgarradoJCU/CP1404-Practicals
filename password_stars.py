8aa055f4a994b0ca8362cd05f601d426d73e6976
JonathanAgarradoJCU <118592614+JonathanAgarradoJCU@users.noreply.github.com>
2022-11-21 09:32:00 +0800

    Add password check program

--- password_stars.py
+++ password_stars.py
@@ -5,9 +5,27 @@ The program should then print asterisks as long as the word.
 Example: if the user enters Pythonista (10 characters), the program should print **********.
 """
 
-
 PASSWORD_LENGTH = 8  # minimum length of typical password
 
+
+def main():
+    user_password = input("Input password: ")  # ask user for input
+    validate_password(user_password)  # validate input
+    print_stars(user_password)  # print stars
+
+
+def validate_password(user_password):
+    while len(user_password) < PASSWORD_LENGTH:
+        print("Minimum password length is 8 characters!")
+        user_password = input("Input password: ")
+
+
+def print_stars(user_password):
+    for i in range(1, len(user_password) + 1, 1):
+        print("*", end='')
+
+
+'''
 user_password = input("Input password: ")
 while len(user_password) < PASSWORD_LENGTH:
     print("Minimum password length is 8 characters!")
@@ -15,3 +33,6 @@ while len(user_password) < PASSWORD_LENGTH:
 
 for i in range(1, len(user_password) + 1, 1):
     print("*", end='')
+'''
+
+main()
