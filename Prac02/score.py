aa492750df9766842b9347420699f4372d69f01f
JonathanAgarradoJCU <118592614+JonathanAgarradoJCU@users.noreply.github.com>
2022-11-22 12:42:37 +0800

    Generate and print random score

--- Prac02/score.py
+++ Prac02/score.py
@@ -1,19 +1,50 @@
 """
 Name: Jonathan Roi Ferrer Agarrado (14275360)
-CP1404/CP5632 - Practical
-Broken program to determine score status
+CP1404/CP5632 - Practical 02
 """
 
-#  new
+import random
 
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
-    score = float(input("Enter score: "))
 
-if score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-else:
-    print("Excellent")
+def main():
+    user_score = get_score()
+    print()
+    while validate_score(user_score) is False:
+        user_score = get_score()
+    print(f"Inputted score generated: {user_score:.0f}%")
+    print(calculate_result(user_score))
+
+
+def display_random_score():
+    random_score = random.randint(0, 100)
+    print(f"Random score generated: {random_score:.0f}%")
+    print(calculate_result(random_score))
+
+
+def get_score():
+    user_score = float(input("Input score: "))
+    return user_score
+
+
+def validate_score(score):
+    if score < 0 or score > 100:
+        print("Invalid score.")
+        return False
+
+
+def calculate_result(score):
+    if score < 50:
+        return "Bad"
+    elif score < 75:
+        return "Passable"
+    elif score < 85:
+        return "Decent"
+    elif score < 100:
+        return "Excellent"
+    else:
+        return "Perfect"
+
+
+main()
+print()
+display_random_score()
