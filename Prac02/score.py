8485e0c6d5cf64eb5ad10b63e5e14ed0ffe06cb6
JonathanAgarradoJCU <118592614+JonathanAgarradoJCU@users.noreply.github.com>
2022-11-22 12:48:29 +0800

    Generate and print user score and random score

--- Prac02/score.py
+++ Prac02/score.py
@@ -2,18 +2,18 @@ import random
 
 
 def main():
-    user_score = get_score()
+    user_score = get_score()  # get user's input as score
     print()
-    while validate_score(user_score) is False:
+    while validate_score(user_score) is False:  # validate user's score
         user_score = get_score()
-    print(f"Inputted score generated: {user_score:.0f}%")
-    print(calculate_result(user_score))
+    print(f"Inputted score generated: {user_score:.0f}%")  # display user's score
+    print(calculate_result(user_score))  # display user's result
 
 
 def display_random_score():
-    random_score = random.randint(0, 100)
-    print(f"Random score generated: {random_score:.0f}%")
-    print(calculate_result(random_score))
+    random_score = random.randint(0, 100)  # get random score
+    print(f"Random score generated: {random_score:.0f}%")  # display random score
+    print(calculate_result(random_score))  # display random score result
 
 
 def get_score():
