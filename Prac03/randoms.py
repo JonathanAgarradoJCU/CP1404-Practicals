107a5a40923d7179a2772e00495db0b7f95a4c37
JonathanAgarradoJCU <118592614+JonathanAgarradoJCU@users.noreply.github.com>
2022-11-27 22:23:29 +0800

    Added random print function

--- Prac03/randoms.py
+++ Prac03/randoms.py
@@ -6,3 +6,5 @@ import random
 is 3 and the steps the random number could generate was 2"""
 
 """The smallest number I could have seen was 2.5 while the largest number I could have seen was 5.5"""
+
+print(random.randint(1, 100))
