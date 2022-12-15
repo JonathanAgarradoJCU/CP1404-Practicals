c3a1de13474b048c51d798812ce2a085dc026842
JonathanAgarradoJCU <118592614+JonathanAgarradoJCU@users.noreply.github.com>
2022-12-15 09:29:13 +0800

    Completed Practical 5 wimbledon.py

--- Prac05/wimbledon.py
+++ Prac05/wimbledon.py
@@ -3,6 +3,7 @@ COUNTRIES = {}
 champions = []
 countries = []
 
+
 def main():
     with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
         data = in_file.readlines()
@@ -15,6 +16,7 @@ def main():
         sorted_countries = sort_list(COUNTRIES)
         print(f"These {len(sorted_countries)} countries have won the Wimbledon:")
         print(", ".join(sorted_countries))
+
 
 def sort_list(dictionary):
     list_current = dictionary.keys()
