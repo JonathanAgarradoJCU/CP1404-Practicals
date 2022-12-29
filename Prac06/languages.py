d38fff6e902960a0f0ac47da5c18c4b6e032e572
JonathanAgarradoJCU <118592614+JonathanAgarradoJCU@users.noreply.github.com>
2022-12-29 15:11:04 +0800

    Edited spacing

--- Prac06/languages.py
+++ Prac06/languages.py
@@ -6,12 +6,10 @@ Actual time : 40 minutes
 
 from Prac06.programming_language import ProgrammingLanguage
 
-
 python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 print(python)
-
 
 programming_languages = [python, ruby, visual_basic]
 dynamic_language = [language for language in programming_languages if language.is_dynamic()]
