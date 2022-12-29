afdfdb271813d63bccf9398663587d9c7cfa395c
JonathanAgarradoJCU <118592614+JonathanAgarradoJCU@users.noreply.github.com>
2022-12-29 15:09:18 +0800

    Put estimated and actual times for all projects

--- Prac06/used_cars.py
+++ Prac06/used_cars.py
@@ -3,7 +3,7 @@ CP1404/CP5632 Practical - Client code to use the Car class.
 Note that the import has a folder (module) in it.
 This is why we name our folders with no spaces or capitals, as valid module names.
 Estimated time : 30 minutes
-Actual time : 25 minutes
+Actual time : 26 minutes
 """
 
 
